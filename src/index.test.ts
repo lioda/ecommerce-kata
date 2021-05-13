f629cbc8a0832b5899db8716700b0b320f6c2d8a
Lioda <lionel.dulout@gmail.com>
2021-05-13 18:19:35 +0200

    feat: kata done with OOP paradigm

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,65 @@
-import { computeCart } from '.'
+import { Item, Cart, ItemWithPricePerKilo, ItemWithUnitPrice, ItemWithDiscountOffer, ItemWithOneFreeOffer } from '.'
 
 describe('computeCart', () => {
-    test('when cart is empty then it should return 0', () => {
-        expect(computeCart()).toBe(0)
+    function itemAtCost(cost: number): Item {
+        return {
+            cost: () => cost
+        }
+    }
+    describe('Cart', () => {
+        test('when cart is empty then it should cost 0', () => {
+            expect(new Cart().cost()).toBe(0)
+        })
+        test('when cart has items then it should cost total of items', () => {
+            const cart = new Cart()
+            cart.addItem(itemAtCost(145))
+            cart.addItem(itemAtCost(12))
+            cart.addItem(itemAtCost(0.89))
+            expect(cart.cost()).toBe(145 + 12 + 0.89)
+        })
     })
+
+    describe('ItemWithUnitPrice', () => {
+        test('when many items then return price multiplied by count', () => {
+            expect(new ItemWithUnitPrice(0.75, 2).cost()).toBe(2 * 0.75)
+        })
+    })
+    describe('ItemWithPricePerKilo', () => {
+        test('when more than 2 decimals then round up', () => {
+            expect(new ItemWithPricePerKilo(1.99, 1230).cost()).toBe(2.45)
+        })
+    })
+    describe('ItemWithDiscountOffer', () => {
+        const discount = { count: 3, newPrice: 2 }
+
+        test('when discount condition is reached then apply it', () => {
+            expect(new ItemWithDiscountOffer({ price: 0.75, count: 3, discount }).cost()).toBe(2)
+        })
+        test('when can apply discount many times then multiply new price', () => {
+            expect(new ItemWithDiscountOffer({ price: 0.75, count: 9, discount }).cost()).toBe(2 * 3)
+        })
+        test('when discount condition is not reach then apply cost per unit', () => {
+            expect(new ItemWithDiscountOffer({ price: 0.75, count: 2, discount }).cost()).toBe(0.75 * 2)
+        })
+        test('when more items than discount condition then apply it and count other items by unit', () => {
+            expect(new ItemWithDiscountOffer({ price: 0.75, count: 5, discount }).cost()).toBe(0.75 * 2 + 2)
+        })
+    })
+    describe('ItemWithOneFreeOffer', () => {
+        const offer = { thresholdForFree: 2 }
+
+        test('when threshold condition is reached then apply it', () => {
+            expect(new ItemWithOneFreeOffer({ price: 1.5, count: 3, offer }).cost()).toBe(1.5 * 2)
+        })
+        test('when threshold condition is not reached then apply cost per unit', () => {
+            expect(new ItemWithOneFreeOffer({ price: 1.5, count: 2, offer }).cost()).toBe(1.5 * 2)
+        })
+        test('when more items than threshold condition then apply cost per unit for remaining items', () => {
+            expect(new ItemWithOneFreeOffer({ price: 1.5, count: 4, offer }).cost()).toBe(1.5 * 2 + 1.5)
+        })
+        test('when can apply offer many times then apply it as needed', () => {
+            expect(new ItemWithOneFreeOffer({ price: 1.5, count: 6, offer }).cost()).toBe(1.5 * 4)
+        })
+    })
+
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,67 @@
-export function computeCart(): number {
-    return 0
+export class Cart {
+    private total: number = 0
+    addItem(item: /*{ item: Item, price: number, count?: number, weight?: number }*/Item) {
+        this.total += item.cost()
+    }
+    cost(): number {
+        return this.total
+    }
+}
+
+export interface Item {
+    cost(): number
+}
+
+export class ItemWithUnitPrice implements Item {
+    constructor(private readonly price: number, private readonly count: number) { }
+
+    cost(): number {
+        return this.price * this.count
+    }
+}
+export class ItemWithPricePerKilo implements Item {
+    constructor(private readonly priceAtKg: number, private readonly grams: number) { }
+
+    cost(): number {
+        return Number((this.priceAtKg * this.grams / 1000).toFixed(2))
+    }
+}
+
+type Discount = { count: number, newPrice: number }
+export class ItemWithDiscountOffer implements Item {
+    private price: number
+    private count: number
+    private discount: Discount
+    constructor({ price, count, discount }:
+        { price: number, count: number, discount: Discount }) {
+        this.price = price
+        this.count = count
+        this.discount = discount
+    }
+
+    cost(): number {
+        const discountApply = Math.floor(this.count / this.discount.count)
+        return this.discount.newPrice * discountApply +
+            this.price * (this.count - discountApply * this.discount.count)
+    }
+}
+
+type ThresholdForFreeOffer = { thresholdForFree: number }
+export class ItemWithOneFreeOffer implements Item {
+    private price: number
+    private count: number
+    private offer: ThresholdForFreeOffer
+    constructor({ price, count, offer }:
+        { price: number, count: number, offer: ThresholdForFreeOffer }) {
+        this.price = price
+        this.count = count
+        this.offer = offer
+    }
+
+    cost(): number {
+        const batchSize = this.offer.thresholdForFree + 1
+        const countBatch = Math.floor(this.count / batchSize)
+        return this.price * this.offer.thresholdForFree * countBatch
+            + this.price * (this.count - countBatch * batchSize)
+    }
 }
