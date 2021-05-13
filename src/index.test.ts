f130426985f8f3ec7f5356fc68f52e0ca5de425c
Lioda <lionel.dulout@gmail.com>
2021-05-13 20:58:53 +0200

    feat: kata done with FP paradigm

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,51 @@
-import { computeCart } from '.'
+import { computeCart, computeItem, Item } from '.'
 
 describe('computeCart', () => {
-    test('when cart is empty then it should return 0', () => {
-        expect(computeCart()).toBe(0)
+    describe('computeCart', () => {
+
+        const computeAsPrice = (item: Item) => item.price
+        test('when cart is empty then it should return 0', () => {
+            expect(computeCart(computeAsPrice, [])).toBe(0)
+        })
+        test('when cart has items then it apply computation for each and return total', () => {
+            expect(computeCart(computeAsPrice, [
+                { price: 0.75, count: 1 },
+                { price: 1.5, count: 1 },
+                { price: 12, count: 1 }
+            ])).toBe(0.75 + 1.5 + 12)
+        })
+    })
+
+    test('when item has price and cound then multiply price and count', () => {
+        expect(computeItem({ price: 0.75, count: 2 })).toEqual(0.75 * 2)
+    })
+    test('when item has weight then multiply price per kilo and weight in kilo with fixed precision', () => {
+        expect(computeItem({ price: 1.99, weight: 1230 })).toEqual(2.45 /* 1.99 * 1.230 */)
+    })
+    describe('specialOffer', () => {
+
+        test('when item has special offer then apply it', () => {
+            expect(computeItem({ price: 0.75, count: 3, specialOffer: { price: 2, size: 3 } })).toEqual(2)
+        })
+
+        test('when not enough items for special offer then not apply it', () => {
+            expect(computeItem({ price: 0.75, count: 2, specialOffer: { price: 2, size: 3 } })).toEqual(0.75 * 2)
+        })
+
+        test('when more items than special offer then count remaining items by unit', () => {
+            expect(computeItem({ price: 0.75, count: 8, specialOffer: { price: 2, size: 3 } })).toEqual(2 * 2 + 0.75 * 2)
+        })
+    })
+
+    describe('oneItemFree', () => {
+        test('when items has one item free then count one less', () => {
+            expect(computeItem({ price: 1.5, count: 3, oneFree: { size: 2 } })).toEqual(1.5 * 2)
+        })
+        test('when not enough items then cost is by unit', () => {
+            expect(computeItem({ price: 1.5, count: 2, oneFree: { size: 2 } })).toEqual(1.5 * 2)
+        })
+        test('when many bundles then apply one free for them and count remaining items by unit', () => {
+            expect(computeItem({ price: 1.5, count: 7, oneFree: { size: 2 } })).toEqual(1.5 * 2 * 2 + 1.5)
+        })
     })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
-export function computeCart(): number {
-    return 0
+export type Item = {
+    price: number
+} & (
+        { weight: number } |
+        { count: number, specialOffer?: { price: number, size: number }, oneFree?: { size: number } }
+    )
+
+export function computeCart(itemCost: (i: Item) => number, items: Item[]): number {
+    return items.map(itemCost).reduce((acc, cost) => acc + cost, 0)
+}
+
+export function computeItem(item: Item): number {
+    if ("weight" in item) {
+        return Number((item.weight / 1000 * item.price).toFixed(2))
+    }
+    let costForOffers = 0
+    let count = item.count
+    if (item.specialOffer && item.count >= item.specialOffer.size) {
+        const countOffers = Math.floor(item.count / item.specialOffer.size)
+        count -= countOffers * item.specialOffer.size
+        costForOffers = item.specialOffer.price * countOffers
+    }
+    if (item.oneFree) {
+        const batchSize = item.oneFree.size + 1
+        const countOffers = Math.floor(item.count / batchSize)
+        count -= countOffers * batchSize
+        costForOffers = countOffers * item.oneFree.size * item.price
+    }
+    return costForOffers + item.price * count
 }
